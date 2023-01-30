b27b390c64f383a910f88f858099563e04cbfe1d
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-01-30 11:58:21 +0000

    layout file complete

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+
+
+
+const Navbar:React.FC = () => {
+    
+    return (
+        <Flex bg="white" height="44px" padding="6px 12px">
+            Navbar
+        </Flex>
+    )
+}
+export default Navbar;
