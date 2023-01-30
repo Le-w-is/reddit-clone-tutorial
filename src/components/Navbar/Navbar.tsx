01ba9beed44954c34f4d46843a8cd20acab980c2
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-01-30 13:54:29 +0000

    button global themes done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
 
-
-
-const Navbar:React.FC = () => {
-    
-    return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-            Navbar
-        </Flex>
-    )
-}
+const Navbar: React.FC = () => {
+	return (
+		<Flex bg="white" height="44px" padding="6px 12px">
+			<Flex align="center">
+				<Image alt="reddit logo" src="/images/redditFace.svg" height="30px" />
+				<Image
+					alt="reddit text logo"
+					src="/images/redditText.svg"
+					height="46PX"
+					display={{ base: "none", md: "unset" }}
+				/>
+			</Flex>
+			<SearchInput />
+			{/* <Directory /> */}
+			<RightContent />
+		</Flex>
+	);
+};
 export default Navbar;
---
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/button";
+import React from "react";
+
+const AuthButtons: React.FC = () => {
+	return (
+		<>
+			<Button>Log In</Button>
+			<Button>Sign Up</Button>
+		</>
+	);
+};
+export default AuthButtons;
