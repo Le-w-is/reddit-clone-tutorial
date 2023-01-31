19fe16c8482c400509c22e2b52a2bff93bfa1b2b
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-01-31 11:25:53 +0000

    signup and login error handling complete

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,18 @@
-import { Flex, Button, Image } from "@chakra-ui/react";
+import { Flex, Button, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+	const [signInWithgoogle, user, loading, error] = useSignInWithGoogle(auth);
 	return (
 		<Flex direction="column" width="100%" mb={4}>
-			<Button variant="oauth" mb={2}>
+			<Button
+				variant="oauth"
+				mb={2}
+				isLoading={loading}
+				onClick={() => signInWithgoogle()}
+			>
 				<Image
 					alt="google logo"
 					src="/images/googlelogo.png"
@@ -14,6 +22,7 @@ const OAuthButtons: React.FC = () => {
 				Continue with Google
 			</Button>
 			<Button variant="oauth">some other provider</Button>
+			{error && <Text>{error.message}</Text>}
 		</Flex>
 	);
 };
--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,4 +1,8 @@
 export const FIREBASE_ERRORS = {
 	"Firebase: Error (auth/email-already-in-use).":
 		"A user with that email already exists",
+	"Firebase: Error (auth/user-not-found).": "Invalid email or password",
+	"Firebase: Error (auth/wrong-password).": "Invalid email or password",
 };
+
+// dont tell user if email or password is wrong, incase it is malicious user
