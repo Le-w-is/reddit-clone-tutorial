e248280fea5d018f97ebd8ae592c79e42e70e131
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-01-31 12:18:16 +0000

    log out button appears when there is a user

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
 	Flex,
 	Modal,
@@ -13,15 +13,25 @@ import { useRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
 import AuthInputs from "./AuthInputs";
 import OAuthButtons from "./OAuthButtons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/clientApp";
 
 const AuthModal: React.FC = () => {
 	const [modalState, setModalState] = useRecoilState(authModalState);
+	const [user, loading, error] = useAuthState(auth);
+
 	const handleClose = () => {
 		setModalState((prev) => ({
 			...prev,
 			open: false,
 		}));
 	};
+
+	useEffect(() => {
+		if (user) handleClose();
+		console.log("user", user);
+		//eslint-disable-next-line
+	}, [user]);
 
 	return (
 		<>
