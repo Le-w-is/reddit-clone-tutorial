c70f431c9a24aaf94d2845997684ceb5a8b92092
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-02-14 14:55:26 +0000

    server side rendering for communities implemented, community not found implemented

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,45 @@
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import { Community } from "../../../atoms/communityAtoms";
+import { firestore } from "../../../firebase/clientApp";
+import safeJsonStringify from "safe-json-stringify";
+import CommunityNotFound from "../../../components/Community/NotFound";
+
+type CommunityPageProps = {
+	communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+	if (!communityData) {
+		return <CommunityNotFound></CommunityNotFound>;
+	}
+	return <div>Welcome to {communityData.id}</div>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	// get community data and pass to client
+	try {
+		const communityDocRef = doc(
+			firestore,
+			"communities",
+			context.query.communityId as string
+		);
+		const communityDoc = await getDoc(communityDocRef);
+
+		return {
+			props: {
+				communityData: communityDoc.exists()
+					? JSON.parse(
+							safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+					  )
+					: "",
+			},
+		};
+	} catch (error) {
+		//could add error page here
+		console.log("getServerSideProps.error", error);
+	}
+}
+
+export default CommunityPage;
