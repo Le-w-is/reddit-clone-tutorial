c9717d0c46a2d42aef1db0cf5d88fe131f408d88
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-02-16 07:13:36 +0000

    if you click on join when not logged in, it prompts you to log in

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -7,7 +7,8 @@ import {
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { authModalState } from "../atoms/authModalAtom";
 import {
 	Community,
 	CommunitySnippet,
@@ -19,6 +20,7 @@ const useCommunityData = () => {
 	const [user] = useAuthState(auth);
 	const [communityStateValue, setCommunityStateValue] =
 		useRecoilState(communityState);
+	const setAuthModalState = useSetRecoilState(authModalState);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState("");
 
@@ -26,6 +28,12 @@ const useCommunityData = () => {
 		communityData: Community,
 		isJoined: boolean
 	) => {
+		if (!user) {
+			setAuthModalState({ open: true, view: "login" });
+			return;
+		}
+
+		setLoading(true);
 		if (isJoined) {
 			leaveCommunity(communityData.id);
 			return;
